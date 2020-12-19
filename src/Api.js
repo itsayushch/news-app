f8e49c5d93ac78d0c4ab8143caeaa555b3255624
Ayush Chowdhury <ayush.chowdhury2004@gmail.com>
2020-12-19 10:49:09 +0530

    using a proxy to avoid CORS error

--- src/Api.js
+++ src/Api.js
@@ -1,11 +1,11 @@
 export async function allArticles() {
-    const res = await fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.REACT_APP_APIKEY}`)
+    const res = await fetch(`https://news-api-proxy.vercel.app/v2/top-headlines?country=in&apiKey=${process.env.REACT_APP_APIKEY}`)
     const data = await res.json();
     return data;
 }
 
 export async function getArticle(source) {
-    const res = await fetch(`https://newsapi.org/v2/everything?sources=${source}&apiKey=${process.env.REACT_APP_APIKEY}`)
+    const res = await fetch(`https://news-api-proxy.vercel.app/v2/everything?sources=${source}&apiKey=${process.env.REACT_APP_APIKEY}`)
     const data = await res.json();
 
     return data;
